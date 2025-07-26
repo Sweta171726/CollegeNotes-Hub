546bdbbcfd1af287369548e3e9992cf135048440
Sweta171726 <swetapriya09876@gmail.com>
2025-07-26 12:28:41 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+   name: { type: String, required: true },
   rollNumber: { type: String, required: true, unique: true },
   branch: { type: String, required: true },
   password: { type: String, required: true },
