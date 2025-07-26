70ea3f6703738246591eb448951e7ee5fdd58f00
Sweta171726 <swetapriya09876@gmail.com>
2025-07-26 12:33:26 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,14 +3,14 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
-    const { rollNumber, branch, password, isAdmin } = req.body;
+    const { name,rollNumber, branch, password, isAdmin } = req.body;
 
   try {
     const userExists = await User.findOne({ rollNumber });
     if (userExists) return res.status(400).json({ msg: "User already exists" });
 
     const hashed = await bcrypt.hash(password, 10);
-    const user = new User({ rollNumber, branch, password: hashed, isAdmin });
+    const user = new User({ name,rollNumber, branch, password: hashed, isAdmin });
 
 
     await user.save();
@@ -30,7 +30,7 @@ exports.login = async (req, res) => {
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
 
     const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: "1d" });
-    res.json({ token, user: { rollNumber: user.rollNumber, branch: user.branch, isAdmin: user.isAdmin } });
+    res.json({ token, user: {name: user.name,rollNumber: user.rollNumber, branch: user.branch, isAdmin: user.isAdmin } });
   } catch (err) {
     res.status(500).json({ msg: "Error in login" });
   }
